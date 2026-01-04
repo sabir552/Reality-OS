b1125ad42c252d8381c2b6892d9568fcaaa7ef16
sabir552 <145090746+sabir552@users.noreply.github.com>
2026-01-05 02:31:17 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,8 +99,9 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.compose.material3:material3:1.2.0")
+
+   implementation("androidx.navigation:navigation-compose:2.7.6")
 
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
