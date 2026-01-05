5e1070bf3163ce659cd31024be0c09535d22edb2
sabir552 <145090746+sabir552@users.noreply.github.com>
2026-01-05 09:57:27 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false // THIS VERSION IS NOW CORRECT
-    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    // This KSP version is compatible with Kotlin 1.9.22
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
